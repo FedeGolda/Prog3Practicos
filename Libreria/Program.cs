5fdc42a9b135f641fa34eb39532558568f0a2af3
FedeGolda <federico.goldaracena@hotmail.com>
2024-04-24 22:39:03 -0300

    Agregado Libreria

---
+++ Libreria/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.SqlClient;
+
+namespace Libreria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=DESKTOP-FIK9LM4;Initial Catalog=Libreria;Integrated Security=True";
+            string query = "INSERT INTO Libro(titulo, descripcion, ID_Autor) VALUES(@param1, @param2, @param3)";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@param1", "Titulo del nuevo libro");
+                    cmd.Parameters.AddWithValue("@param2", "Descripcion del nuevo libro");
+                    cmd.Parameters.AddWithValue("@param3", 1);
+
+                    try
+                    {
+                        conn.Open();
+                        var nFilas = cmd.ExecuteNonQuery();
+                        if (nFilas != 1)
+                            Console.WriteLine("Error en el numero de filas afectadas");
+
+                        conn.Close();
+
+                        Console.ReadLine();
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                }
+            }
+
+        }
+    }
