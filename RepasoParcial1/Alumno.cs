37dba7db59916cb18d4da38a20ee7d82d6ab0849
FedeGolda <federico.goldaracena@hotmail.com>
2024-05-01 17:18:04 -0300

    Agregado y repasando para el parcial

---
+++ RepasoParcial1/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoParcial1
+{
+    internal class Alumno
+    {
+        public string Nombre { set; get; }
+        public int Edad { set; get; }
+        public int Nota { set; get; }
+
+        public Alumno(string nombre, int edad, int nota)
+        {
+            this.Nombre = nombre;
+            this.Edad = edad;
+            this.Nota = nota;
+        }
+    }
+}
---
+++ RepasoParcial1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RepasoParcial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alumno> alumnos = new List<Alumno>()
+            {
+                new Alumno("Eva", 20, 6),
+                new Alumno("Ana", 18, 7),
+                new Alumno("Rosa", 22, 5),
+                new Alumno("Ricardo", 30, 9),
+                new Alumno("Felipe", 45, 2),
+                new Alumno("Pepe", 19, 3),
+                new Alumno("Laia", 26, 10),
+                new Alumno("Stephanie", 33, 6),
+                new Alumno("Agustin", 50, 7),
+                new Alumno("Mauricio", 31, 12)
+            };
+
+           // var alumnosExonerados = new List<Alumno>();
+
+            // Usando LINQ para filtrar y ordenar la lista de alumnos
+            var alumnosExonerados = alumnos
+                .Where(alumno => alumno.Nota > 6 && alumno.Nombre.Length > 4)
+                .OrderBy(alumno => alumno.Edad)
+                .ToList();
+
+
+            foreach (var alumno in alumnos)
+            {
+                if (alumno.Nota > 6 && alumno.Nombre.Length > 4)
+                {
+                    alumnosExonerados.Add(alumno);
+                }
+            }
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+            // Ordenamiento burbuja simplificado
+            bool huboCambio;
+            do
+            {
+                huboCambio = false;
+                for (int i = 0; i < alumnosExonerados.Count - 1; i++)
+                {
+                    if (alumnosExonerados[i].Edad > alumnosExonerados[i + 1].Edad)
+                    {
+                        var temp = alumnosExonerados[i];
+                        alumnosExonerados[i] = alumnosExonerados[i + 1];
+                        alumnosExonerados[i + 1] = temp;
+                        huboCambio = true;
+                    }
+                }
+            } while (huboCambio);
+
+            foreach (var alumno in alumnosExonerados)
+            {
+                Console.WriteLine($"Nombre: {alumno.Nombre} - Edad: {alumno.Edad} - Nota: {alumno.Nota}");
+            }
+
+
+        }
+    }
+}
