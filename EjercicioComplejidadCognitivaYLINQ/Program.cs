24efe2fcf69be518290ae6a5009b7108e738bb30
FedeGolda <federico.goldaracena@hotmail.com>
2024-05-01 23:21:03 -0300

    Agregado y terminado

---
+++ EjercicioComplejidadCognitivaYLINQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+namespace EjercicioComplejidadCognitivaYLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var valores = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // var sumaPar = 0;
+            // var sumaImpar = 0;
+            // var suma = 0;
+
+            var sumaPar = valores.Where(v => v % 2 == 0).Sum();
+            var sumaImpar = valores.Where(v => v % 2 != 0).Sum();
+            var sumaTotal = valores.Sum();
+            var promedio = valores.Average();
+
+            /*
+            foreach (var valor in valores) +1
+            {
+                if (valor % 2 == 0) 1 + 1
+                {
+                    sumaPar += valor;
+                }
+
+                if (valor % 2 != 0) +1
+                {
+                    sumaImpar += valor;
+                }
+                suma += valor;
+            }
+
+            var promedio = 0;
+            if (suma > 0) +1
+            {
+                promedio = suma / valores.Count;
+            } 
+            Total Complejidad: 5
+            */ 
+
+            Console.WriteLine(promedio);
+            Console.WriteLine(sumaPar);
+            Console.WriteLine(sumaImpar);
+        }
+    }
+}
---
+++ EjercicioComplejidadCognitivaYLINQ2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EjercicioComplejidadCognitivaYLINQ2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> valores = new List<int>() { 3, 6, 9, 7, 4, 1, 2, 5, 8 };
+
+            var valoresOrdenados = valores.OrderByDescending(x => x).ToList();
+            var valorMaximo = valoresOrdenados.First();
+
+            for (var indice = 0; indice < valores.Count - 1; indice++) // +1
+            {
+                if (valores[indice] > valores[indice + 1]) // 1 + 1
+                {
+                    var valorTemporal = valores[indice];
+                    valores[indice] = valores[indice + 1];
+                    valores[indice + 1] = valorTemporal;
+                    indice = -1;
+                }
+            }
+
+            Console.WriteLine("Valores ordenados manualmente:");
+            foreach (var valorOrdenado in valores) // +1
+            {
+                Console.WriteLine(valorOrdenado);
+            }
+
+            Console.WriteLine("\nValores ordenados con LINQ:");
+            foreach (var valorOrdenadoLinq in valoresOrdenados) // +1
+            {
+                Console.WriteLine(valorOrdenadoLinq);
+            }
+
+            var mayor = 0;
+            foreach (var valor in valores) // +1
+            {
+                if (valor > mayor) // +1
+                {
+                    mayor = valor;
+                }
+            }
+
+            Console.WriteLine("\nEl valor más grande es (sin LINQ): " + mayor);
+            Console.WriteLine("El valor más grande es (con LINQ): " + valorMaximo);
+        }
+    }
+}
