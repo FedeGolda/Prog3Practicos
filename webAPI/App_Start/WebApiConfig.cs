0f6de639e17bd652c8c382b356136c0dd3bb552f
FedeGolda <federico.goldaracena@hotmail.com>
2024-05-28 21:56:39 -0300

    Agregado y creado webAPI y pdf

---
+++ webAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace webAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Remover formato XML de los formatos soportados por el web API
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+        }
+    }
+}
---
+++ webAPI/Controllers/AutoController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Sockets;
+
+namespace webAPI.Controllers
+{
+    public class AutoController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetAutorById(long id)
+        {
+
+        }
+    }
+}
