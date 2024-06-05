363f5490bd93b72504aa8422f89c2f19224da641
FedeGolda <federico.goldaracena@hotmail.com>
2024-06-05 17:21:42 -0300

    Terminado de agregar controllers y pestañas (navbars) en Layout

---
+++ ObligatorioProg3/Controllers/ReseñasController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ObligatorioProg3.Models;
+
+namespace ObligatorioProg3.Controllers
+{
+    public class ReseñasController : Controller
+    {
+        private readonly ObligatorioP3Context _context;
+
+        public ReseñasController(ObligatorioP3Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Reseñas
+        public async Task<IActionResult> Index()
+        {
+            var obligatorioP3Context = _context.Reseñas.Include(r => r.Cliente).Include(r => r.Restaurante);
+            return View(await obligatorioP3Context.ToListAsync());
+        }
+
+        // GET: Reseñas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reseña = await _context.Reseñas
+                .Include(r => r.Cliente)
+                .Include(r => r.Restaurante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reseña == null)
+            {
+                return NotFound();
+            }
+
+            return View(reseña);
+        }
+
+        // GET: Reseñas/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id");
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id");
+            return View();
+        }
+
+        // POST: Reseñas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ClienteId,RestauranteId,Puntaje,Comentarios,FechaReseña")] Reseña reseña)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reseña);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", reseña.ClienteId);
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", reseña.RestauranteId);
+            return View(reseña);
+        }
+
+        // GET: Reseñas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reseña = await _context.Reseñas.FindAsync(id);
+            if (reseña == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", reseña.ClienteId);
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", reseña.RestauranteId);
+            return View(reseña);
+        }
+
+        // POST: Reseñas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ClienteId,RestauranteId,Puntaje,Comentarios,FechaReseña")] Reseña reseña)
+        {
+            if (id != reseña.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reseña);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReseñaExists(reseña.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", reseña.ClienteId);
+            ViewData["RestauranteId"] = new SelectList(_context.Restaurantes, "Id", "Id", reseña.RestauranteId);
+            return View(reseña);
+        }
+
+        // GET: Reseñas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reseña = await _context.Reseñas
+                .Include(r => r.Cliente)
+                .Include(r => r.Restaurante)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reseña == null)
+            {
+                return NotFound();
+            }
+
+            return View(reseña);
+        }
+
+        // POST: Reseñas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var reseña = await _context.Reseñas.FindAsync(id);
+            if (reseña != null)
+            {
+                _context.Reseñas.Remove(reseña);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReseñaExists(int id)
+        {
+            return _context.Reseñas.Any(e => e.Id == id);
+        }
+    }
+}
