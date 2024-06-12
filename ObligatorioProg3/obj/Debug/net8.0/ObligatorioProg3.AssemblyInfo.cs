a64ef22d9d3bb40978589547a5d7e6d81532419f
FedeGolda <federico.goldaracena@hotmail.com>
2024-06-12 20:10:11 -0300

    Probando Login en Notebook (FUNCIONA)

--- ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
+++ ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67fb7527c1cd9a2f62220882fd84d5acc67ef9bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+393a223608a8d51d0e980b245e1ba3d93b85388f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
