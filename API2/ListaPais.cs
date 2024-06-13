e4ef5b05f076ca6f73f1053e03649477ab3cf171
FedeGolda <federico.goldaracena@hotmail.com>
2024-06-12 22:40:32 -0300

    Agregado API2

---
+++ API2/ListaPais.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace API2
+{
+
+    public partial class ListaPais
+    {
+        [JsonProperty("success", NullValueHandling = NullValueHandling.Ignore)]
+        public long? Success { get; set; }
+
+        [JsonProperty("result", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Pais>Paises { get; set; }
+    }
+
+    public partial class Pais
+    {
+        [JsonProperty("country_key", NullValueHandling = NullValueHandling.Ignore)]
+        public long? CountryKey { get; set; }
+
+        [JsonProperty("country_name", NullValueHandling = NullValueHandling.Ignore)]
+        public string? CountryName { get; set; }
+
+        [JsonProperty("country_iso2")]
+        public string? CountryIso2 { get; set; }
+
+        [JsonProperty("country_logo")]
+        public Uri? CountryLogo { get; set; }
+    }
+}
+
---
+++ API2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace API2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new RestClient("https://apiv2.allsportsapi.com/");
+            var request = new RestRequest("football?met=Countries&APIkey=ad1d734557f4af2afe23faf9accbdc44d061617c06072d0713d54d49333f0730", Method.Get);
+            RestResponse response = client.Execute(request);
+            if (response.IsSuccessStatusCode)
+            {
+                ListaPais misPaises = JsonConvert.DeserializeObject<ListaPais>(response.Content);
+            }
+            else 
+            {
+                Console.WriteLine(response.Content);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
