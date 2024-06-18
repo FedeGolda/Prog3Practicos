456596410cee7c0bde830b7c63b0f4565949dc57
FedeGolda <federico.goldaracena@hotmail.com>
2024-06-18 20:07:51 -0300

    Agregado ObligatorioP3_V2.bak

--- ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
+++ ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4db1d0fb1a3f7180bb31d160b5f5838b34ee52d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+953bb9ca25364c0e880e0158e162246d0a3e92bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
