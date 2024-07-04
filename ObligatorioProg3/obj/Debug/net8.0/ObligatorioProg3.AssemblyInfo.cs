ece12635e04641662c89cd949649c243d21f185c
FedeGolda <federico.goldaracena@hotmail.com>
2024-07-03 21:05:00 -0300

    agregando direccion local sql server de notebook

--- ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
+++ ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cc3fbbf7efb74bb14903721d89fdfd2bd965f62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d74a85e53b9dec4eccf1df549557db66ab558fe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
