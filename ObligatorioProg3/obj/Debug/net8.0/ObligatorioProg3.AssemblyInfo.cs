e61a95b1146c274a1dbc458246baaa9f693a18f4
FedeGolda <federico.goldaracena@hotmail.com>
2024-07-10 18:41:06 -0300

    corrigiendo cruds en las tablas en index

--- ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
+++ ObligatorioProg3/obj/Debug/net8.0/ObligatorioProg3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c86674491a6938cf82db573a50cc01a123f4be8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e9f6dbb1ba898c01038d9b3b3992d03dafb1fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObligatorioProg3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
